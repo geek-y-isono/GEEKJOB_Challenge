b6a5fa92a56c1b236b843aac8615ae2945f91123
Yui-PC\Yui <s06015yi@gmai.com>
2017-02-14 22:37:38 +0900

    オブジェクト指向1-4_クラスの継承_課題提出します

---
+++ CampChallenge_java/005.オブジェクト指向１/クラスの継承/initdata.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kadai4;
+
+/**
+ *
+ * @author Yui
+ */
+public class initdata extends goods{
+    public void setInit(){
+        name = "データなし";
+        stock = 0;
+    }
+}
