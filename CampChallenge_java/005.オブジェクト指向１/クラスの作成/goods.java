089504c737e423f079c7f4916c3775d7dcabe719
Yui-PC\Yui <s06015yi@gmai.com>
2017-02-14 22:37:18 +0900

    オブジェクト指向1-3_クラスの作成_課題提出します

---
+++ CampChallenge_java/005.オブジェクト指向１/クラスの作成/goods.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kadai3;
+
+import java.io.PrintWriter;
+/**
+ *
+ * @author Yui
+ */
+public class goods {
+    public String name;
+    public Integer stock;
+    public void setGoods(String n,Integer s){
+        name = n;
+        stock = s;
+    }
+    public void printgoods(PrintWriter out){
+        out.print("商品名:"+name+"<br>"+"在庫:"+stock);
+    }
+}
